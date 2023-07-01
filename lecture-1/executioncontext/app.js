e7266ecbb0fd319ed5ab8b86e94781529242cadc
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-07-01 21:45:57 +0530

    first commit

---
+++ lecture-1/executioncontext/app.js
@@ -0,0 +1,26 @@
+
+
+let username = 'yogesh';
+
+function yogiji(){
+    let a = 10;
+    function modiji(){
+        console.log(a);
+    }
+    modiji();
+    return a;
+}
+
+let c = yogiji();
+
+console.log(username);
+console.log(c);
+
+
+
+
+
+
+
+
+
---
+++ lecture-1/objects/app.js
@@ -0,0 +1,26 @@
+
+// let obj = {
+//     a: 10,
+//     b: 20,
+//     c: 30
+// }
+
+// console.log(obj['a']);
+// console.log(obj.a);
+// for(let item in obj){
+    // console.log(item);
+    // console.log(obj[item]);
+// }
+
+// console.log(obj);
+
+
+// let arr = [10,20,30,'sam' , 'samarth' , undefined , null , [101,102,[1001,1002]]];
+
+// console.log(arr[7][2][0]);
+// for(let item of arr){
+//     console.log(item);
+// }
+
+
+
