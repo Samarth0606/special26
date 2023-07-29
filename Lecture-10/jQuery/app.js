75e9d7bcf8d28fccc8289915e8cc9bc7c8fac4fb
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-07-29 20:00:10 +0530

    lecture 10 codes

---
+++ Lecture-10/jQuery/app.js
@@ -0,0 +1,86 @@
+// $('.sam').css('color' , 'red');
+// $('.sam').css('background-color' , 'green');
+// $('.sam').css('font-size' , '54px');
+
+// --------------------------------
+
+// $('h1').css('color' , 'red');
+// $('h1').css('background-color' , 'green');
+// $('h1').css('font-size' , '54px');
+
+
+// ------------------------------
+
+// $('h1').css({
+//     "color": "red",
+//     "backgroundColor" : "yellow",
+//     backgroundColor : "yellow",
+//     "background-color" : "blue",
+//     // background-color : "green",
+// });
+
+// ---------------------------------
+
+// accessing a text from an element
+
+
+// console.log($('h1').text()) ;
+// console.log($('h1').text()) ;
+// console.log($('.sam').text()) ;
+
+
+// console.log($('h1').first().text()) ;
+
+// console.log($('h1').last().text()) ; //getter
+
+// ---------------------------------------
+
+// console.log($('h1').last().text('sam')) ; //setter
+
+// console.log($('h1').last().text('<h1>samarth</h1>')) ; //setter
+// console.log($('h1').last().html('<h1>samarth</h1>')) ; //setter
+
+// --------------------------------------
+
+// console.log($('a')) 
+
+// console.log( $('a').attr('href') )  //getter
+
+// console.log( $('a').attr('href' , 'http://facebook.com') )  //setter
+
+
+
+// --------------------------------------
+
+// console.log($('input').val());
+
+
+// console.log($('h1').first().text()) ;
+
+// let h1 = $('h1:nth-of-type(2)').text();
+// // h1:nth-of-type(2)).text() ;
+// console.log(h1)
+
+// -------------------------------------
+
+//class
+
+// $('h1').addClass('heading')
+$('h1').addClass('heading second');
+
+$('h1').removeClass('heading second');
+
+$('h1').toggleClass('togglee')
+
+console.log($('h1').hasClass('togglee')) 
+
+
+
+
+
+
+
+
+
+
+
