6fa5c0fd17c2b84ba7d959f91da4ceb6442ba144
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-10-08 09:54:01 +0530

    lecture 30 code

---
+++ Lecture-30/Ecommerce-special26/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const seedDB = require('./seed');
+const productRoutes = require('./routes/productRoutes');
+// const ejsMate = require('ejs-mate')
+
+// database connection
+mongoose.set('strictQuery' , true); //version 7 ki vajah se
+mongoose.connect('mongodb://127.0.0.1:27017/special26') //returns a promise
+.then(()=>{console.log("DB CONNECTED")})
+.catch((err)=>{console.log("error in DB" , err)})
+
+//setting templates
+// app.engine('ejs' , ejsMate)
+app.set('view engine' , 'ejs')
+app.set('views' , path.join(__dirname , 'views'));
+// setting static files
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// using all the routes in order to verify the path an run the function
+app.use(productRoutes);
+
+
+// adding dummy data to the collection
+// seedDB()
+
+
+// running on port
+const PORT = 8080;
+app.listen(PORT , ()=>{
+    console.log(`server connected at port: ${PORT}`);
+})
---
+++ Lecture-30/Ecommerce-special26/routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router(); //mini instance/application;
+
+
+// READ
+router.get('/products' , async (req,res)=>{
+    let allProducts = await Product.find();
+    res.render('product/index' , {allProducts})
+})
+
+// 
+
+// export so that you can use it in app.ja
+module.exports = router;
